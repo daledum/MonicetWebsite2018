9e13535dc102d0c0ea2412a4a754f5ffc41bac99
Francisco Cardoso <franciscocardoso@morfose.net>
2010-03-12 14:34:37 -0100

    .

--- apps/frontend/modules/contacts/templates/indexSuccess.php
+++ apps/frontend/modules/contacts/templates/indexSuccess.php
@@ -1,5 +1,5 @@
-<p class="content"><?php echo __('...'); ?></p>
-<p class="content"><?php echo $webmaster; ?></p>
+<p class="content"><?php echo __('The easiest way to reach us is by e-mail. Just write to %1% or use the form below', array('%1%' => $webmaster)); ?></p>
+<br />
 <?php if (! $sf_user->getFlash('success')): ?>
 <form action="<?php echo url_for('contacts/index') ?>" method="POST">
   <table>
@@ -15,3 +15,19 @@
 <?php else: ?>
 <span class="success"><?php echo __('Your message was successfully sent to Monicet'); ?></span>
 <?php endif; ?>
+<br />
+<p class="content"><?php echo __('More formal communication can be made to:'); ?></p>
+<ul class="content">
+    <li><?php echo __('MONICET project'); ?></li>
+    <li><?php echo __('c/o'); ?> Prof. Ana Isabel Neto</li>  
+</ul>
+<ul class="content">
+    <li>CIRN, Universidade dos Açores</li>
+    <li>R. Mãe de Deus, 13A</li>
+    <li>9501-801 Ponta Delgada</li>
+    <li><?php echo __('Azores'); ?>, Portugal</li>
+</ul>
+<ul class="content">
+    <li><?php echo __('Phone'); ?>: + 351 296 650 102</li>
+    <li>Fax: + 351 296 650 100</li>
+</ul>
