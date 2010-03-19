3d441b4a4b29f757b96dfbe58c58edfd2690af2b
Tito Miguel Costa <titomiguelcosta@morfose.net>
2010-03-19 11:01:57 -0100

    alteração nos links para abrir em nova página

--- apps/frontend/modules/consorcium/templates/showSuccess.php
+++ apps/frontend/modules/consorcium/templates/showSuccess.php
@@ -1,10 +1,10 @@
 <h2><?php echo $element->getName(); ?></h2>
 <?php if($element->getLogotype()): ?>
-    <?php echo link_to_if($element->getLink(), image_tag('/uploads/consorcium/tn_'.$element->getLogotype(), 'align=left vspace=10 hspace=10 width=140px alt_title='.$element->getName()), $element->getLink()) ?>
+    <?php echo link_to_if($element->getLink(), image_tag('/uploads/consorcium/tn_'.$element->getLogotype(), 'align=left vspace=10 hspace=10 width=140px alt_title='.$element->getName()), $element->getLink(), 'target=_blank') ?>
 <?php endif ?>
-<br />
 <?php if($element->getLink() && !$element->getLogotype()): ?>
     <p><a href="<?php echo $element->getLink(); ?>" target="_blank"><?php echo $element->getLink() ?></a></p>
+    <br />
 <?php endif ?>
 <?php if($element->getDescription()): ?>
     <div class="content"><?php echo $element->getDescription(ESC_RAW); ?></div>
