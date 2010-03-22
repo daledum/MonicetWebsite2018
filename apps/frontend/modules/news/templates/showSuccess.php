fa4ad4ec9d56d749215d1a5cc634f37ba444638f
Tito Miguel Costa <titomiguelcosta@morfose.net>
2010-03-22 18:14:11 -0100

    alteração nos templates das notícias para mostrar data da publicacao

--- apps/frontend/modules/news/templates/showSuccess.php
+++ apps/frontend/modules/news/templates/showSuccess.php
@@ -1,5 +1,5 @@
 <p class="_p_article_headline"><?php echo $article->getHeadline(ESC_RAW) ?></p>
-<p class="_p_article_date"><?php echo $article->getUpdatedAt() ?></p>
+<p class="_p_article_date"><?php echo $article->getPublishedDate() ?></p>
 <?php if($article->getImage()) echo image_tag('/uploads/news/tn_' . $article->getImage(), 'align=left vspace=10 hspace=10 width="140px" alt_title=' . $article->getHeadline()); ?>
 <div class="_p_article_body"><?php echo nl2br($article->getBody(ESC_RAW)) ?></div>
 
