aae48a8ccd1b91a18c19539de44abd1d1221ec6d
Tito Miguel Costa <titomiguelcosta@notebook>
2010-07-11 16:34:59 +0000

    link

--- apps/backend/modules/record/lib/recordGeneratorHelper.class.php
+++ apps/backend/modules/record/lib/recordGeneratorHelper.class.php
@@ -10,4 +10,8 @@
  */
 class recordGeneratorHelper extends BaseRecordGeneratorHelper
 {
+  public function linkToTeste($object, $params)
+  {
+    return '<a href="http://www.morfose.net/?nome='.$object.'">'.$object.'</a>';
+  }
 }
