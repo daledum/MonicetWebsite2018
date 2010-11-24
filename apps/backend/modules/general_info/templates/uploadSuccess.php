baae71be8425d412cf74a1d845775988e0b07520
morfose <morfose@morfose-7>
2010-11-24 16:29:43 -0100

    botoes import e export

--- apps/backend/modules/general_info/templates/uploadSuccess.php
+++ apps/backend/modules/general_info/templates/uploadSuccess.php
@@ -3,6 +3,7 @@
   <form>
     <b style="margin: 10px;" >Seleccione o ficheiro a importar (.xls):</b>
     <br />
+    
     <input style="margin: 10px;" type="file" id="ficheiroexp" />
     <ul class="sf_admin_actions">
       <li><input type="submit" value="importar" /></li>
