73183db8f042226059aab98d4e7976f263e8d34a
morfose <morfose@morfose-7>
2010-11-24 16:28:56 -0100

    correccao datas e coordenadas das saidas

---
+++ apps/backend/modules/general_info/templates/indexSuccess.php
@@ -0,0 +1,36 @@
+<?php use_helper('I18N', 'Date') ?>
+<?php include_partial('general_info/assets') ?>
+
+<div id="sf_admin_container">
+  <h1><?php echo __('General info List', array(), 'general_info') ?></h1>
+
+  <?php include_partial('general_info/flashes') ?>
+
+  <div id="sf_admin_header">
+    <?php include_partial('general_info/list_header', array('pager' => $pager)) ?>
+  </div>
+
+  <div id="sf_admin_bar">
+    <?php include_partial('general_info/filters', array('form' => $filters, 'configuration' => $configuration)) ?>
+  </div>
+
+  <div id="sf_admin_content">
+    <form action="<?php echo url_for('general_info_collection', array('action' => 'batch')) ?>" method="post">
+    <?php include_partial('general_info/list', array('pager' => $pager, 'sort' => $sort, 'helper' => $helper)) ?>
+    <ul class="sf_admin_actions">
+      <?php include_partial('general_info/list_batch_actions', array('helper' => $helper)) ?>
+      <?php include_partial('general_info/list_actions', array('helper' => $helper)) ?>
+      <li class="sf_admin_action_export">
+        <a href="#">Exportar</a>
+      </li>
+      <li class="sf_admin_action_import">
+        <a href="<?php echo url_for('general_info/upload') ?>">Importar</a>
+      </li>
+    </ul>
+    </form>
+  </div>
+
+  <div id="sf_admin_footer">
+    <?php include_partial('general_info/list_footer', array('pager' => $pager)) ?>
+  </div>
+</div>
---
+++ apps/backend/modules/general_info/templates/uploadSuccess.php
@@ -0,0 +1,11 @@
+<div id="sf_admin_container">
+  <h1>Importar ficheiro</h1>
+  <form>
+    <b style="margin: 10px;" >Seleccione o ficheiro a importar (.xls):</b>
+    <br />
+    <input style="margin: 10px;" type="file" id="ficheiroexp" />
+    <ul class="sf_admin_actions">
+      <li><input type="submit" value="importar" /></li>
+    </ul>
+  </form>
+</div>
