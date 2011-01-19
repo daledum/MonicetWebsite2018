2f23c79e683b5a56ec384fd406b39d766a79ab78
António Couto <antonio.pedro.couto@morfose.net>
2011-01-19 15:10:23 -0100

    alteracoes ficheiro exportado

--- apps/backend/modules/general_info/actions/actions.class.php
+++ apps/backend/modules/general_info/actions/actions.class.php
@@ -227,30 +227,71 @@ class general_infoActions extends autoGeneral_infoActions
     
     // edição do conteúdo do ficheiro
       // headers
-    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(0,1, 'Data');
-    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(1,1, 'Código');
-    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(2,1, 'Hora');
-    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(3,1, 'Latitude');
-    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(4,1, 'Longitude');
-    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(5,1, 'Beaufort');
-    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(6,1, 'Visibilidade');
-    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(7,1, 'Espécie');
-    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(8,1, 'Tamanho Grupo');
-    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(9,1, 'Ad');
-    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(10,1, 'Ju');
-    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(11,1, 'Crias');
-    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(12,1, 'Comportamento');
-    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(13,1, 'Associações');
-    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(14,1, 'Nº Embarcações');
-    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(15,1, 'Comentários');
-    $objPHPExcel->getActiveSheet()->getStyle('A1:P1')->getFont()->setBold(true);
-    $letras = array('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P');
+    
+    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(3,1, 'Position /EFF');
+    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(12,1, 'Sighting');
+    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(19,1, 'Inf. Geral');
+    
+    $objPHPExcel->getActiveSheet()->getStyle('A1')->getFill()
+      ->setFillType(PHPExcel_Style_Fill::FILL_SOLID)
+      ->getStartColor()->setRGB('0000ff');
+    
+    //$objPHPExcel->getActiveSheet()->mergeCells('B1:E1');
+    $objPHPExcel->getActiveSheet()->getStyle('C1:E1')->getBorders()->getLeft()->setBorderStyle(PHPExcel_Style_Border::BORDER_NONE);
+    $objPHPExcel->getActiveSheet()->getStyle('B1:E1')->getFill()
+      ->setFillType(PHPExcel_Style_Fill::FILL_SOLID)
+      ->getStartColor()->setRGB('ffff99');
+    
+    //$objPHPExcel->getActiveSheet()->mergeCells('F1:G1');
+    $objPHPExcel->getActiveSheet()->getStyle('G1')->getBorders()->getLeft()->setBorderStyle(PHPExcel_Style_Border::BORDER_NONE);
+    $objPHPExcel->getActiveSheet()->getStyle('F1:G1')->getFill()
+      ->setFillType(PHPExcel_Style_Fill::FILL_SOLID)
+      ->getStartColor()->setRGB('0000ff');
+    
+    //$objPHPExcel->getActiveSheet()->mergeCells('H1:P1');
+    $objPHPExcel->getActiveSheet()->getStyle('I1:P1')->getBorders()->getLeft()->setBorderStyle(PHPExcel_Style_Border::BORDER_NONE);
+    $objPHPExcel->getActiveSheet()->getStyle('H1:P1')->getFill()
+      ->setFillType(PHPExcel_Style_Fill::FILL_SOLID)
+      ->getStartColor()->setRGB('ff9900');
+    
+    //$objPHPExcel->getActiveSheet()->mergeCells('Q1:V1');
+    $objPHPExcel->getActiveSheet()->getStyle('R1:V1')->getBorders()->getLeft()->setBorderStyle(PHPExcel_Style_Border::BORDER_NONE);
+    $objPHPExcel->getActiveSheet()->getStyle('Q1:V1')->getFill()
+      ->setFillType(PHPExcel_Style_Fill::FILL_SOLID)
+      ->getStartColor()->setRGB('0000ff');
+    
+      
+    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(0,2, 'Data');
+    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(1,2, 'Código');
+    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(2,2, 'Hora');
+    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(3,2, 'Latitude');
+    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(4,2, 'Longitude');
+    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(5,2, 'Visibilidade');
+    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(6,2, 'Beaufort');
+    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(7,2, 'Espécie');
+    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(8,2, 'Tamanho Grupo');
+    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(9,2, 'Ad');
+    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(10,2, 'Ju');
+    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(11,2, 'Crias');
+    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(12,2, 'Comportamento');
+    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(13,2, 'Associações');
+    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(14,2, 'Nº Embarcações');
+    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(15,2, 'Comentários');
+    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(16,2, 'Empresa');
+    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(17,2, 'Barco');
+    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(18,2, 'Skipper');
+    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(19,2, 'Biologist');
+    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(20,2, 'Passg');
+    $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(21,2, 'Dist Precorrida');
+    $objPHPExcel->getActiveSheet()->getStyle('A1:V1')->getFont()->setBold(true);
+    $objPHPExcel->getActiveSheet()->getStyle('A2:V2')->getFont()->setBold(true);
+    $letras = array('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V');
     foreach($letras as $letra){
       $objPHPExcel->getActiveSheet()->getColumnDimension($letra)->setAutoSize(true);
     }
     
       // conteudo
-    $l = 2;
+    $l = 3;
     foreach($dados as $gi){
       $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(0,$l, $gi->getDate());
       $records = RecordPeer::doSelectRecordsByGeneralInfoId($gi->getId());
@@ -278,8 +319,8 @@ class general_infoActions extends autoGeneral_infoActions
         $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(2,$l, $record->getTime());
         $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(3,$l, $record->getLatitude());
         $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(4,$l, $record->getLongitude());
-        $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(5,$l, $record->getSeaStateId());
-        $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(6,$l, $record->getVisibilityId());
+        $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(5,$l, $record->getVisibilityId());
+        $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(6,$l, $record->getSeaStateId());
         $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(7,$l, $specie);
         $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(8,$l, $sighting->getTotal());
         $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(9,$l, $sighting->getAdults());
@@ -289,6 +330,10 @@ class general_infoActions extends autoGeneral_infoActions
         $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(13,$l, $association);
         $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(14,$l, $record->getNumVessels());
         $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(15,$l, $sighting->getComments());
+        if($gi->getCompany()) $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(16,$l, $gi->getCompany()->getName());
+        if($gi->getVessel()) $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(17,$l, $gi->getVessel()->getName());
+        if($gi->getSkipper()) $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(18,$l, $gi->getSkipper()->getName());
+        if($gi->getGuide()) $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(19,$l, $gi->getGuide()->getName());
         $l++;
       }
     }
