bcc3e33612a6eed8bfd182a4e6dc708929bc5324
António Couto <antonio.pedro.couto@morfose.net>
2011-05-27 15:25:47 +0000

    pagina para download da exportacao

---
+++ apps/backend/modules/general_info/templates/downloadSuccess.php
@@ -0,0 +1,14 @@
+<div id="sf_admin_container">
+  <h1>Exportar ficheiro</h1>
+  <b style="margin: 10px;" >Seleccione o ano que deseja exportar:</b>
+  <br />
+  <fieldset id="sf_fieldset_none"></fieldset>
+  <div class="sf_admin_form_row">
+    <div>
+      <label for="year">Download file:</label>
+      <div class="content">
+        <a href="<?php echo $filedir ?>"><?php echo $filename ?></a>
+      </div>
+    </div>
+  </div>
+</div>
