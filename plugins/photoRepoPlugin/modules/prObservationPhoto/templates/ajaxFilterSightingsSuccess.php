d850ed78b0aba12d31915ef2d95db837fbeda80a
Hugo Vilarinho <vilarinhoctc@gmail.com>
2012-12-10 16:51:11 -0100

    some fixes requested by Axel

--- plugins/photoRepoPlugin/modules/prObservationPhoto/templates/ajaxFilterSightingsSuccess.php
+++ plugins/photoRepoPlugin/modules/prObservationPhoto/templates/ajaxFilterSightingsSuccess.php
@@ -2,7 +2,6 @@
 <?php
   //$sightings = $sf_data->getRaw('sightings');
   //echo print_r($sightings);
-echo $OBPhoto->getSightingId();
   $options = '';
   foreach($sightings as $key => $value){
     $options .= '<option value="' . $key . '"'.( ($OBPhoto->getSightingId() == $key)? ' selected="selected"': '' ).'>' . $value. '</option>';
