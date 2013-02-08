bd4335f73fa572e9c471c1890bae056f69282201
Hugo Vilarinho <vilarinhoctc@gmail.com>
2013-02-08 17:36:08 -0100

    Added next and previous navigation on observation photo edit page.

--- plugins/photoRepoPlugin/modules/prObservationPhoto/templates/editSuccess.php
+++ plugins/photoRepoPlugin/modules/prObservationPhoto/templates/editSuccess.php
@@ -4,6 +4,14 @@
 <div id="sf_admin_container" class="pendent_photo_container">
   <h1><?php echo __('A editar uma fotografia de observação de cetáceos', array(), 'messages') ?></h1>
 
+  <?php if(isset($prevId) || isset($nextId)): ?>
+    <ul class="sf_admin_actions">
+      <?php if(isset($prevId)): ?><li class="sf_admin_action_left"><a href="<?php echo url_for('@pr_observation_photo_edit?id='.$prevId) ?>">Anterior</a></li><?php endif; ?>
+      <?php if(isset($nextId)): ?><li class="sf_admin_action_right"><a href="<?php echo url_for('@pr_observation_photo_edit?id='.$nextId) ?>">Seguinte</a></li><?php endif; ?>
+    </ul><br/>
+  <?php endif; ?>
+  
+
   <?php include_partial('prObservationPhoto/flashes') ?>
 
   <div id="sf_admin_header">
