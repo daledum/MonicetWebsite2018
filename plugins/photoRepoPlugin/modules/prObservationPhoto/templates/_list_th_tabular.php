d01e4dcd916e6b1ad467864273dd73f0533e8d3f
Hugo Vilarinho <vilarinhoctc@gmail.com>
2013-11-14 09:26:11 -0100

    Added field best to pentdent observattioPhoto and catalog list page.

--- plugins/photoRepoPlugin/modules/prObservationPhoto/templates/_list_th_tabular.php
+++ plugins/photoRepoPlugin/modules/prObservationPhoto/templates/_list_th_tabular.php
@@ -14,6 +14,16 @@
 </th>
 <?php end_slot(); ?>
 <?php include_slot('sf_admin.current_header') ?><?php slot('sf_admin.current_header') ?>
+<th class="sf_admin_boolean sf_admin_list_th_is_best">
+  <?php if ('is_best' == $sort[0]): ?>
+    <?php echo image_tag('/mfAdministracaoPlugin/images/icons/'.$sort[1].'.png', array('alt' => __($sort[1], array(), 'sf_admin'), 'title' => __($sort[1], array(), 'sf_admin'))) ?>
+    <?php echo link_to(__('Is best', array(), 'observation_photo'), '@pr_observation_photo', array('query_string' => $template_query_string.'sort=is_best&sort_type='.($sort[1] == 'asc' ? 'desc' : 'asc'))) ?>
+  <?php else: ?>
+    <?php echo link_to(__('Is best', array(), 'observation_photo'), '@pr_observation_photo', array('query_string' => $template_query_string.'sort=is_best&sort_type=asc')) ?>
+  <?php endif; ?>
+</th>
+<?php end_slot(); ?>
+<?php include_slot('sf_admin.current_header') ?><?php slot('sf_admin.current_header') ?>
 <th width="120" class="sf_admin_foreignkey sf_admin_list_th_individual_id">
   <?php if ('individual_id' == $sort[0]): ?>
     <?php echo image_tag('/mfAdministracaoPlugin/images/icons/'.$sort[1].'.png', array('alt' => __($sort[1], array(), 'sf_admin'), 'title' => __($sort[1], array(), 'sf_admin'))) ?>
