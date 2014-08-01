5d27871254ef12543f00e2d31e370447b61b6dbc
Alex Dumitrescu <alex_samprasno1@yahoo.co.uk>
2014-08-01 16:08:56 +0000

    Fixed bug 4 - for Saida, users cannot add info related to animals when on codes I or F

--- apps/backend/modules/record/templates/lineSuccess.php
+++ apps/backend/modules/record/templates/lineSuccess.php
@@ -21,13 +21,13 @@
   <td class="sf_admin_text longitude"><?php echo $record_form['longitude']; ?></td>
   <td class="sf_admin_text sea_state"><?php echo $record_form['sea_state_id']; ?></td>
   <td class="sf_admin_text visibility"><?php echo $record_form['visibility_id']; ?></td>
-  <td class="sf_admin_text specie"><?php echo $sighting_form['specie_id']; ?></td>
-  <td class="sf_admin_text total"><?php echo $sighting_form['total']; ?></td>
-  <td class="sf_admin_text adults"><?php echo $sighting_form['adults']; ?></td>
-  <td class="sf_admin_text juveniles"><?php echo $sighting_form['juveniles']; ?></td>
-  <td class="sf_admin_text calves"><?php echo $sighting_form['calves']; ?></td>
-  <td class="sf_admin_text behaviour"><?php echo $sighting_form['behaviour_id']; ?></td>
-  <td class="sf_admin_text association"><?php echo $sighting_form['association_id']; ?></td>
+  <td class="sf_admin_text specie"><?php if($n_lines != 1) { echo $sighting_form['specie_id']; } ?></td>
+  <td class="sf_admin_text total"><?php if($n_lines != 1) { echo $sighting_form['total']; } ?></td>
+  <td class="sf_admin_text adults"><?php if($n_lines != 1) { echo $sighting_form['adults']; } ?></td>
+  <td class="sf_admin_text juveniles"><?php if($n_lines != 1) { echo $sighting_form['juveniles']; } ?></td>
+  <td class="sf_admin_text calves"><?php if($n_lines != 1) { echo $sighting_form['calves']; } ?></td>
+  <td class="sf_admin_text behaviour"><?php if($n_lines != 1) { echo $sighting_form['behaviour_id']; } ?></td>
+  <td class="sf_admin_text association"><?php if($n_lines != 1) { echo $sighting_form['association_id']; } ?></td>
   <td class="sf_admin_text num_vessels"><?php echo $record_form['num_vessels']; ?></td>
   <td class="sf_admin_text comments"><?php echo $sighting_form['comments']; ?></td>
   <td class="sf_admin_text remove"><div class="remove-line-div" style="margin-top: 10px;"><a href="#" class="remove-line"><img src="/images/backend/icons/garbage.png" width="20"></a></div></td>
@@ -50,9 +50,18 @@
         url: url[0] + "//" + url[2] + "/" + url[3] + "/record/lineAjax",
         data: { valor: $("#record_code_id_<?php echo $n_lines ?>").val(), '_r': Math.random()*100 },
         success: function(html){
-          //if( $("#record_code_id_<?php echo $n_lines ?>").val() != 2 ){
+          if( $("#record_code_id_<?php echo $n_lines ?>").val() != 2 ){
             $("#record_code_id_<?php echo $n_lines+1 ?>").append(html);
-          //}
+          }
+          else {  
+                  $(".record_line_<?php echo $n_lines ?> #sighting_specie_id").remove();  
+                  $(".record_line_<?php echo $n_lines ?> #sighting_total").remove();
+                  $(".record_line_<?php echo $n_lines ?> #sighting_adults").remove();
+                  $(".record_line_<?php echo $n_lines ?> #sighting_juveniles").remove();
+                  $(".record_line_<?php echo $n_lines ?> #sighting_calves").remove();
+                  $(".record_line_<?php echo $n_lines ?> #sighting_behaviour_id").remove();
+                  $(".record_line_<?php echo $n_lines ?> #sighting_association_id").remove();                
+            }  
         },
         error: function(html, text, codigo){
           alert("erro");
