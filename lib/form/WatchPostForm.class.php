54b80f3a971affa03345b7846ca260a9a5c69f14
Alex Dumitrescu <alex_samprasno1@yahoo.co.uk>
2014-10-06 15:29:13 +0000

    Coordinates validation improvement - WatchPostForm

--- lib/form/WatchPostForm.class.php
+++ lib/form/WatchPostForm.class.php
@@ -23,31 +23,51 @@ class WatchPostForm extends BaseWatchPostForm
         $this->setWidget('company_id', 
                          new sfWidgetFormChoice(array('choices' => array($company->getId() => $company->getName()))));
 
-      $base = GeoLocation::fromDegrees( $company->getBaseLatitude(), $company->getBaseLongitude() );
-      $coordinates = $base->boundingCoordinates(100, 'kilometers');
-
-      $maximum_lat = $coordinates[1]->getLatitudeInDegrees();
-      $minimum_lat = $coordinates[0]->getLatitudeInDegrees();
-      $this->validatorSchema['latitude'] = new sfValidatorNumber(
-        array( 'max' => $maximum_lat,
-               'min' => $minimum_lat
-        ),
-        array( 'max' => 'Latitude can only take values between '. $minimum_lat. ' and '. $maximum_lat,
-               'min' => 'Latitude can only take values between '. $minimum_lat. ' and '. $maximum_lat
-        )
-      );
-    
-      $maximum_long = $coordinates[1]->getLongitudeInDegrees();
-      $minimum_long = $coordinates[0]->getLongitudeInDegrees();
-      $this->validatorSchema['longitude'] = new sfValidatorNumber(
-        array( 'max' => $maximum_long,
-               'min' => $minimum_long
-        ),
-        array( 'max' => 'Longitude can only take values between '. $minimum_long. ' and '. $maximum_long,
-               'min' => 'Longitude can only take values between '. $minimum_long. ' and '. $maximum_long
-        )
-      );
-
+        $this->validatorSchema->setPostValidator(
+        new sfValidatorCallback(array('callback' => array($this, 'validateCoordinates')))
+        );
     }
   }
+
+  public function validateCoordinates($validator, $values)
+  {
+    
+    $company_obj = CompanyPeer::retrieveByPk($values['company_id']);
+
+    if($company_obj && $values['latitude'] && $values['longitude']){
+
+      $lat = trim($values['latitude'], " ");
+      $long = trim($values['longitude'], " ");
+
+      $base = GeoLocation::fromDegrees( $company_obj->getBaseLatitude(), $company_obj->getBaseLongitude() );
+      $coordinates = $base->boundingCoordinates(100, 'kilometers');
+      $maximum_long = $coordinates[1]->getLongitudeInDegrees();
+      $minimum_long = $coordinates[0]->getLongitudeInDegrees();
+      $maximum_lat = $coordinates[1]->getLatitudeInDegrees();
+      $minimum_lat = $coordinates[0]->getLatitudeInDegrees();
+      
+      if( (!is_numeric($lat) || $lat < $minimum_lat || $lat > $maximum_lat) && (!is_numeric($long) || $long < $minimum_long || $long > $maximum_long) ){
+        throw new sfValidatorErrorSchema($validator, array(
+        'latitude' => new sfValidatorError($validator, 'Latitude can only take decimal degrees format (no degrees, minutes or seconds symbols, please) values between '.$minimum_lat.' and '.$maximum_lat),
+        'longitude' => new sfValidatorError($validator, 'Longitude can only take decimal degrees format (no degrees, minutes or seconds symbols, please) values between '.$minimum_long.' and '.$maximum_long)
+        ));
+      }
+      else{
+            if( !is_numeric($lat) || $lat < $minimum_lat || $lat > $maximum_lat ){
+                throw new sfValidatorErrorSchema($validator, array(
+                'latitude' => new sfValidatorError($validator, 'Latitude can only take decimal degrees format (no degrees, minutes or seconds symbols, please) values between '.$minimum_lat.' and '.$maximum_lat)
+              ));
+            }
+            else{
+                  if( !is_numeric($long) || $long < $minimum_long || $long > $maximum_long ){
+                      throw new sfValidatorErrorSchema($validator, array(
+                      'longitude' => new sfValidatorError($validator, 'Longitude can only take decimal degrees format (no degrees, minutes or seconds symbols, please) values between '.$minimum_long.' and '.$maximum_long)
+                  ));
+                }
+            }
+          }
+    }
+    return $values;
+  }
+
 }
